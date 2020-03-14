3a1d214e84b12b1b9714f2c21df678aa3ee10c1b
Maxwell Gorley <38299798+gizmo4487@users.noreply.github.com>
2020-03-14 10:03:58 -0400

    Using Discord.JS instead of Discord.io

--- o-bot.js
+++ o-bot.js
@@ -1,60 +1,40 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+const Discord = require('discord.js');
+const auth = require('./auth.json');
 
 // Create bot object
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+const bot = new Discord.Client({autoReconnect:true});
 
 // Display console message when logged in
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.once('ready', () => {
+    console.log('Connected');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', message => {
     // Prefix is '!'
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
 		
 		// Commands
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
+            	// !ping
+            	case 'ping':
+                	message.channel.send('Pong!');
+            	break;
 			
-			// !help
-			case 'help':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Commands:\n!help: Display this message\n!ping: Test if bot is alive\n!pi: Make O speak'
-				});
-			break;
-			
-			// !pi
-			case 'pi':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Pipi? Pipi-pipipipipi!'
+		// !help
+		case 'help':
+			message.channel.send('Commands:\n!help: Display this message\n!ping: Test if bot is alive\n!pi: Make O speak');
+		break;
 		
-				})
+		// !pi
+		case 'pi':
+			message.channel.send('Pipi? Pipi-pipipipipi!');
 			break;
             // End of command list
          }
      }
 });
+
+bot.login(auth.token);
